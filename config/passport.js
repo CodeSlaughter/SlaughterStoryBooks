3eec3d739a498459c08d9c9fc32f2befc3523d25
Iam Slaughter <jlgslaughter@gmail.com>
2018-04-20 14:55:39 -0400

    wip

--- config/passport.js
+++ config/passport.js
@@ -1,12 +1,12 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
-const keys = require('./keys');
+
 
 module.exports = function(passport){
     passport.use(
         new GoogleStrategy({
-            clientID: process.env.GOOGLE_CLIENT_ID || keys.googleClientID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET || keys.googleClientSecret,
+            clientID: process.env.GOOGLE_CLIENT_ID || require('./keys').googleClientID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || require('./keys').googleClientSecret,
             callbackURL: process.env.GOOGLE_CALLBACK || '/auth/google/callback',
             proxy: true
         }, (accessToken, refreshToken, profile, done) => {
