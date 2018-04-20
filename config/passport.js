110f0cbc703b34c3154302ff7766d079681355af
Iam Slaughter <jlgslaughter@gmail.com>
2018-04-20 14:47:58 -0400

    fixing bugs

--- config/passport.js
+++ config/passport.js
@@ -5,8 +5,8 @@ const keys = require('./keys');
 module.exports = function(passport){
     passport.use(
         new GoogleStrategy({
-            clientID: process.env.GOOGLECLIENTID || keys.googleClienID,
-            clientSecret: process.env.GOOGLECLIENTSECRET || keys.googleClientSecret,
+            clientID: process.env.GOOGLE_CLIENT_ID || keys.googleClientID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || keys.googleClientSecret,
             callbackURL: '/auth/google/callback',
             proxy: true
         }, (accessToken, refreshToken, profile, done) => {
