8795a88ce1dfb330ae552768f8e581daae89c66e
Iam Slaughter <jlgslaughter@gmail.com>
2018-04-20 13:55:34 -0400

    working on google oath

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+//load routes
+const auth = require('./routes/auth');
 
 const app =  express();
 
 const port =  process.env.PORT || 8080;
+
+// load routes
+app.use('/auth', auth);
 
 app.get('/', (req, res) => {
     res.send('it works')
