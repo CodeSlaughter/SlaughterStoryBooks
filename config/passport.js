ccceecd0bf38cd8ee3818ced0433e3f8bd51d067
Iam Slaughter <jlgslaughter@gmail.com>
2018-04-23 15:41:39 -0400

    save user to db functionality

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,7 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
+//Load user model
+const User = mongoose.model('users');
 
 
 module.exports = function(passport){
@@ -10,8 +12,34 @@ module.exports = function(passport){
             callbackURL: process.env.GOOGLE_CALLBACK || '/auth/google/callback',
             proxy: true
         }, (accessToken, refreshToken, profile, done) => {
-            console.log(accessToken);
-            console.log(profile)
+            const image = profile.photos[0].value.substring(0, profile.photos[0].value.indexOf('?'));
+            const newUser = {
+                googleID: profile.id,
+                firstName: profile.name.givenName,
+                lastName: profile.name.lastName,
+                email: profile.emails[0].value,
+                image: image
+            }
+
+            //check for existing user
+            User.findOne({
+                googleID: profile.id
+            })
+            .then((user) => {
+                if (user) {
+                    //return user
+                    done(null, user)
+                } else {
+                    //create user
+                    new User(newUser)
+                    .save()
+                    .then((user) => {
+                        done(null, user)
+                    })
+                    .catch(err => console.log(err));
+                }
+            })
+            .catch(err => console.log(err))
         })
     )
 }
