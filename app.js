09cc7b763bb9264441a1a12e76d51bca34340b69
Iam Slaughter <jlgslaughter@gmail.com>
2018-04-23 18:23:52 -0400

    working on navbar

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const exhbs = require('express-handlebars');
 
 // Load User model
 require('./models/User')
@@ -12,6 +13,7 @@ require('./config/passport')(passport);
 
 //load routes
 const auth = require('./routes/auth');
+const index = require('./routes/index')
 
 //loud keys
 const keys = require('./config/keys')
@@ -29,10 +31,6 @@ const app =  express();
 
 const port =  process.env.PORT || 8080;
 
-// Passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
-
 app.use(cookieParser());
 app.use(session({
     secret: 'secret',
@@ -40,19 +38,24 @@ app.use(session({
     saveUninitialized: false
 }));
 
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 //Set global vars
 app.use((req, res, next) => {
     res.locals.user = req.user || null;
     next();
 });
 
+app.engine('handlebars', exhbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
 // Use routes
 app.use('/auth', auth);
-
-
-app.get('/', (req, res) => {
-    res.send('it works')
-})
+app.use('/', index);
 
 app.listen(port, () => {
     console.log(`server is listening on port ${port}`)
