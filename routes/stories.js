df4e078df9aa93d53f66cfc7f4ead0414b89a4b0
Iam Slaughter <jlgslaughter@gmail.com>
2018-04-25 15:12:16 -0400

    initial app done

--- routes/stories.js
+++ routes/stories.js
@@ -1,21 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const {ensureAuthenticated, ensureGuest} = require('../helpers/auth');
+const { ensureAuthenticated, ensureGuest } = require('../helpers/auth');
 const mongoose = require('mongoose');
-const Story  = mongoose.model('stories');
+const Story = mongoose.model('stories');
 
 //stories index
 router.get('/', (req, res) => {
     Story.find({
         status: 'public'
     })
-    .populate('user')
-    .then((stories) => {
-        res.render('stories/index', {
-            stories: stories
-        });
-    })
-    .catch(err => console.log(err))
+        .populate('user')
+        .sort({
+            date: 'desc'
+        })
+        .then((stories) => {
+            res.render('stories/index', {
+                stories: stories
+            });
+        })
+        .catch(err => console.log(err))
 });
 
 //add stories form
@@ -28,18 +31,56 @@ router.get('/edit/:id', ensureAuthenticated, (req, res) => {
     Story.findOne({
         _id: req.params.id
     })
-    .then((story) => {
-        res.render('stories/edit', {
-            story: story
+        .then((story) => {
+            if (story.user !== req.user.id) {
+                res.redirect('/')
+            } else {
+                res.render('stories/edit', {
+                    story: story
+                })
+            }
         })
-    })
-    .catch(err => console.log(err))
+        .catch(err => console.log(err))
 });
+
+//list stories from a user
+router.get('/user/:userId', (req, res) => {
+    Story.find({
+        user: req.params.userId, status: 'public'
+    })
+        .populate('user')
+        .sort({
+            date: 'desc'
+        })
+        .then((stories) => {
+            res.render('stories/index', {
+                stories: stories
+            })
+        })
+        .catch(err => console.log(err))
+})
+
+//Logged in user story stories
+router.get('/my', ensureAuthenticated, (req, res) => {
+    Story.find({
+        user: req.params.userId
+    })
+        .populate('user')
+        .sort({
+            date: 'desc'
+        })
+        .then((stories) => {
+            res.render('stories/index', {
+                stories: stories
+            })
+        })
+        .catch(err => console.log(err))
+})
 
 //process add stories
 router.post('/', ensureAuthenticated, (req, res) => {
     let allowComments;
-    if(req.body.allowComments){
+    if (req.body.allowComments) {
         allowComments = true;
     } else {
         allowComments = false;
@@ -65,14 +106,29 @@ router.get('/show/:id', (req, res) => {
     Story.findOne({
         _id: req.params.id
     })
-    .populate('user')
-    .populate('comments.commentUser')
-    .then((story) => {
-        res.render('stories/show', {
-            story: story
+        .populate('user')
+        .populate('comments.commentUser')
+        .then((story) => {
+            if (story.status === 'public') {
+                res.render('stories/show', {
+                    story: story
+                })
+            } else {
+                if (req.user) {
+                    if (req.user.id === story.user.id) {
+                        res.render('stories/show', {
+                            story: story
+                        });
+                    } else {
+                        res.redirect('/stories');
+                    }
+                } else {
+                    res.redirect('/stories');
+                }
+            }
+
         })
-    })
-    .catch(err => console.log(err))
+        .catch(err => console.log(err))
 });
 
 //Edit form process
@@ -80,24 +136,24 @@ router.put('/:id', (req, res) => {
     Story.findOne({
         _id: req.params.id
     })
-    .then((story) => {
-        let allowComments;
-        if(req.body.allowComments){
-            allowComments = true;
-        } else {
-            allowComments = false;
-        }
-        story.title = req.body.title;
-        story.body = req.body.body;
-        story.status = req.body.status;
-        story.allowComments = allowComments;
-        story.save()
         .then((story) => {
-            res.redirect('/dashboard')
+            let allowComments;
+            if (req.body.allowComments) {
+                allowComments = true;
+            } else {
+                allowComments = false;
+            }
+            story.title = req.body.title;
+            story.body = req.body.body;
+            story.status = req.body.status;
+            story.allowComments = allowComments;
+            story.save()
+                .then((story) => {
+                    res.redirect('/dashboard')
+                })
+                .catch(err => console.log(err))
         })
         .catch(err => console.log(err))
-    })
-    .catch(err => console.log(err))
 });
 
 //Delete form process
@@ -105,30 +161,30 @@ router.delete('/:id', (req, res) => {
     Story.remove({
         _id: req.params.id
     })
-    .then(()=>{
-        res.redirect('/dashboard')
-    });
+        .then(() => {
+            res.redirect('/dashboard')
+        });
 });
 
 router.post('/comment/:id', (req, res) => {
     Story.findOne({
         _id: req.params.id
     })
-    .then(story => {
-        const newComment = {
-            commentBody: req.body.commentBody,
-            commentUser: req.user.id
-        }
-        //add to comments array
-        story.comments.unshift(newComment);
-
-        story.save()
         .then(story => {
-            res.redirect(`/stories/show/${story.id}`);
+            const newComment = {
+                commentBody: req.body.commentBody,
+                commentUser: req.user.id
+            }
+            //add to comments array
+            story.comments.unshift(newComment);
+
+            story.save()
+                .then(story => {
+                    res.redirect(`/stories/show/${story.id}`);
+                })
+                .catch(err => console.log(err))
         })
-        .catch(err => console.log(err))
-    })
-    .catch(err => console.log(err));
+        .catch(err => console.log(err));
 });
 
 module.exports = router;
